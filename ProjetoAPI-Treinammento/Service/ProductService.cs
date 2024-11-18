0dffc1487447187ee697de0ce40efe0e50bc014b
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-18 11:43:31 -0300

    feat: mongo db service

---
+++ ProjetoAPI-Treinammento/Service/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ProjetoAPI_Treinammento.Data;
+using ProjetoAPI_Treinammento.Models;
+
+namespace ProjetoAPI_Treinammento.Service;
+
+public class ProductService
+{
+    private readonly IMongoCollection<Product> _productCollection;
+
+    public ProductService(IOptions<ProductDbSettings> productService)
+    {
+        var mongoClient = new MongoClient(productService.Value.ConnectionString);
+        var mongoDatabase = mongoClient.GetDatabase(productService.Value.DataBaseName);
+
+        _productCollection = mongoDatabase.GetCollection<Product>(productService.Value.ProductCollectionName);
+    } 
+}
