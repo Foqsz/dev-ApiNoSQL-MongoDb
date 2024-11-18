02dd2701ca91ee7bface6e19b6e38e0413089c77
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-18 11:01:10 -0300

    feat: add model product

---
+++ ProjetoAPI-Treinammento/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProjetoAPI_Treinammento.Models;
+
+public class Product
+{  
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Nome")]
+    public string Nome { get; set; } = string.Empty;
+}
