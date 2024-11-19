4a0486b4f2ae99144098e29f9d14f4cc88bde9d9
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-19 10:11:33 -0300

    refactor: reajustando mongodb

--- ProjetoAPI-Treinammento/Program.cs
+++ ProjetoAPI-Treinammento/Program.cs
@@ -1,14 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
+using ProjetoAPI_Treinammento.Data;
+using ProjetoAPI_Treinammento.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Adicionando configuração para MongoDB
-var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb");
-var mongoClient = new MongoClient(mongoConnectionString);
-builder.Services.AddSingleton<IMongoClient>(mongoClient);
+//var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb");
+//var mongoClient = new MongoClient(mongoConnectionString);
+//builder.Services.AddSingleton<IMongoClient>(mongoClient);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.Configure<ProductDbSettings>
+    (builder.Configuration.GetSection("FoqsDataBase"));
+
+builder.Services.AddSingleton<ProductService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
