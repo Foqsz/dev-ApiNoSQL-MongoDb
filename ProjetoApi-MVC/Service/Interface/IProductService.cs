1abb2f265ed09750812d78f32f990e772bffd780
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-21 11:59:17 -0300

    feat: update product

--- ProjetoApi-MVC/Service/Interface/IProductService.cs
+++ ProjetoApi-MVC/Service/Interface/IProductService.cs
@@ -5,7 +5,6 @@ namespace ProjetoApi_MVC.Service.Interface;
 public interface IProductService
 {
     Task<IEnumerable<ProductViewModel>> GetAsyncProduct();
-    Task<ProductViewModel> GetAsyncProductById(string id);
     Task<ProductViewModel> PostProductService(ProductViewModel product);
     Task<ProductViewModel> UpdateProductService(string id, ProductViewModel product);
     Task DeleteProductService(string id);
