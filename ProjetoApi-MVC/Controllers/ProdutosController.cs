07b5ccd379450cebcb78d0fc2848117a603ef595
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-21 10:32:38 -0300

    feat: add controllers

---
+++ ProjetoApi-MVC/Controllers/ProdutosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoApi_MVC.Models;
+using ProjetoApi_MVC.Service.Interface;
+
+namespace ProjetoApi_MVC.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProdutosController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductViewModel>>> Index()
+        {
+            var produtos = await _productService.GetAsyncProduct();
+
+            if (produtos is null)
+            {
+                return View("Error");
+            }
+
+            return View(produtos);  
+        }
+
+    }
+}
