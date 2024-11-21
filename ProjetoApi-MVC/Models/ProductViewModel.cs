689bef83329aacee703b4c573544a61038fd4806
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-21 10:32:55 -0300

    feat: add models

---
+++ ProjetoApi-MVC/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProjetoApi_MVC.Models;
+
+public class ProductViewModel
+{ 
+    public string? Id { get; set; }
+
+    [BsonElement("Nome")]
+    public string Nome { get; set; } = string.Empty;
+}
